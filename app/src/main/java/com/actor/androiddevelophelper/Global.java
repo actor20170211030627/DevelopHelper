6cfdec46cf898979bb8b98ca76bc787816bb97be
ldf <ldf@qq.com>
2021-01-08 11:23:00 +0800

    优化约束布局计算, remove manifest-merger-debug-report.txt

--- app/src/main/java/com/actor/androiddevelophelper/Global.java
+++ app/src/main/java/com/actor/androiddevelophelper/Global.java
@@ -8,10 +8,11 @@ package com.actor.androiddevelophelper;
  * @version 1.0
  */
 public class Global {
-    public static final String HEIGHT_TITLEBAR = "HEIGHT_TITLEBAR";
-    public static final String HEIGHT_TOTAL = "HIGHT_TOTAL";
-    public static final String HEIGHT_VIEW = "HEIGHT_VIEW";
-    public static final String MARGIN_VIEW = "MARGIN_VIEW";
+    public static final String HEIGHT_TITLEBAR    = "HEIGHT_TITLEBAR";
+    public static final String HEIGHT_TOTAL       = "HIGHT_TOTAL";
+    public static final String HEIGHT_VIEW        = "HEIGHT_VIEW";
+    public static final String MARGIN_VIEW_TOP    = "MARGIN_VIEW_TOP";
+    public static final String MARGIN_VIEW_BOTTOM = "MARGIN_VIEW_BOTTOM";
 
     public static final String CHECK_UPDATE = "https://gitee.com/actor20170211030627/" +
             "DevelopHelper" +
--- app/src/main/java/com/actor/androiddevelophelper/activity/CalculateConstrailtLayoutActivity.java
+++ app/src/main/java/com/actor/androiddevelophelper/activity/CalculateConstrailtLayoutActivity.java
@@ -1,6 +1,7 @@
 package com.actor.androiddevelophelper.activity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -25,8 +26,10 @@ public class CalculateConstrailtLayoutActivity extends BaseActivity {
     EditText etTotalHeight;
     @BindView(R.id.et_view_height)
     EditText etViewHeight;
-    @BindView(R.id.et_view_margin)
-    EditText etViewMargin;
+    @BindView(R.id.et_view_margin_top)
+    EditText etViewMarginTop;
+    @BindView(R.id.et_view_margin_bottom)
+    EditText etViewMarginBottom;
     @BindView(R.id.tv_result)
     TextView tvResult;//计算结果
 
@@ -40,34 +43,57 @@ public class CalculateConstrailtLayoutActivity extends BaseActivity {
         etTitleBarHeight.setText(SPUtils.getString(Global.HEIGHT_TITLEBAR));
         etTotalHeight.setText(SPUtils.getString(Global.HEIGHT_TOTAL));
         etViewHeight.setText(SPUtils.getString(Global.HEIGHT_VIEW));
-        etViewMargin.setText(SPUtils.getString(Global.MARGIN_VIEW));
+        etViewMarginTop.setText(SPUtils.getString(Global.MARGIN_VIEW_TOP));
+        etViewMarginBottom.setText(SPUtils.getString(Global.MARGIN_VIEW_BOTTOM));
     }
 
+    //开始计算
     private void calculate() {
-        if (isNoEmpty(etTotalHeight, etViewHeight, etViewMargin)) {
-            String text1 = getText(etTitleBarHeight);
-            String text2 = getText(etTotalHeight);
-            String text3 = getText(etViewHeight);
-            String text4 = getText(etViewMargin);
-            SPUtils.putString(Global.HEIGHT_TITLEBAR, text1);
-            SPUtils.putString(Global.HEIGHT_TOTAL, text2);
-            SPUtils.putString(Global.HEIGHT_VIEW, text3);
-            SPUtils.putString(Global.MARGIN_VIEW, text4);
+        String marginTopS = getText(etViewMarginTop);//顶部边距
+        String marginBottomS = getText(etViewMarginBottom);//底部边距
 
-            if (isEmpty(text1)) text1 = "0";
-            double titleBarHeight = Double.parseDouble(text1);
-            double totalHeight = Double.parseDouble(text2);
-            double viewHeight = Double.parseDouble(text3);
-            double viewMargin = Double.parseDouble(text4);
-            double leftHeight = totalHeight - titleBarHeight - viewHeight;//剩下宽/高度
-            String result = "0";
-            if (leftHeight <= 0) {
-                tvResult.setText("0");
-            } else result = String.valueOf(viewMargin / leftHeight);
-            String clip = getStringFormat("%s/(%s-%s-%s)=%s", text4, text2, text1, text3, result);
+        //如果2个边距都不为空
+        if (!TextUtils.isEmpty(marginTopS) && !TextUtils.isEmpty(marginBottomS)) {
+            //保存
+            SPUtils.putString(Global.MARGIN_VIEW_TOP, marginTopS);
+            SPUtils.putString(Global.MARGIN_VIEW_BOTTOM, marginBottomS);
+            //计算
+            double marginTop = Double.parseDouble(marginTopS);
+            double marginBottom = Double.parseDouble(marginBottomS);
+            double bias = marginTop / (marginTop + marginBottom);//偏移量
+            String clip = getStringFormat("第2种计算方式: %s/(%s+%s)=%f", marginTopS, marginTopS, marginBottomS, bias);
             tvResult.setText(clip);
             copy2Clipboard(clip);
-            toast("已复制到剪贴板");
+            toast("第2种计算方式, 已复制到剪贴板");
+        } else {
+            String totalHeightS = getText(etTotalHeight);//总高度
+            String viewHeightS = getText(etViewHeight);//控件高度
+            if (!TextUtils.isEmpty(marginTopS) && !TextUtils.isEmpty(totalHeightS) && !TextUtils.isEmpty(viewHeightS)) {
+                String titleBarHeightS = getText(etTitleBarHeight);//标题栏高度
+                //保存
+                SPUtils.putString(Global.HEIGHT_TITLEBAR, titleBarHeightS);
+                SPUtils.putString(Global.HEIGHT_TOTAL, totalHeightS);
+                SPUtils.putString(Global.HEIGHT_VIEW, viewHeightS);
+                SPUtils.putString(Global.MARGIN_VIEW_TOP, marginTopS);
+                if (isEmpty(titleBarHeightS)) titleBarHeightS = "0";
+                double titleBarHeight = Double.parseDouble(titleBarHeightS);
+                double totalHeight = Double.parseDouble(totalHeightS);
+                double viewHeight = Double.parseDouble(viewHeightS);
+                double viewMargin = Double.parseDouble(marginTopS);
+                double leftHeight = totalHeight - titleBarHeight - viewHeight;//剩下宽/高度
+                String result = "0";
+                if (leftHeight == 0) {
+                    tvResult.setText("0");
+                } else {
+                    result = String.valueOf(viewMargin / leftHeight);
+                }
+                String clip = getStringFormat("%s/(%s-%s-%s)=%s", marginTopS, totalHeightS, titleBarHeightS, viewHeightS, result);
+                tvResult.setText(clip);
+                copy2Clipboard(clip);
+                toast("已复制到剪贴板");
+            } else {
+                toast("请输入对应高度, 推荐输入第2种计算方式的高度!");
+            }
         }
     }
 
